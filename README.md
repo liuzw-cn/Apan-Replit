615ff551e373e5ce5ad00b580cffe83142e0310c
溜追 <86212054+liuzhiwei90@users.noreply.github.com>
2023-05-08 22:32:09 +0800

    首次上传

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Apan-Replit
+在Replit部署
+## 部署
+
+点击下方 `run on repl.it` 按钮，将apan快速部署到`repl.it`。
+
+<a href="https://repl.it/github/liuzhiwei90/Apan-Replit">
+  <img alt="Run on Repl.it" src="https://repl.it/badge/github/liuzhiwei90/Apan-Replit" style="height: 40px; width: 190px;" />
+</a>
---
+++ main.sh
@@ -0,0 +1,33 @@
+if [ ! -f "../data.db" ]; then
+    echo "../data.db 不存在。"
+    if [ -n "$url_db" ] && [ -n "$url_db_shm" ] && [ -n "$url_db_wal" ]; then
+        echo "正在下载数据库。"
+        curl -L $url_db -o ../data.db
+        if [ "$?" -ne 0 ]; then
+            echo "data.db下载失败，复制默认数据库。"
+            cp ./data/data.db* ../
+        else
+            echo "data.db下载成功。"
+            curl -L $url_db_shm -o ../data.db-shm
+            if [ "$?" -ne 0 ]; then
+                echo "data.db-shm下载失败。"
+            else
+                echo "data.db-shm下载成功。"
+                curl -L $url_db_wal -o ../data.db-wal
+                if [ "$?" -ne 0 ]; then
+                    echo "data.db-wal下载失败。"
+                else
+                    echo "data.db-wal下载成功。"
+                fi
+            fi
+        fi
+
+    else
+        echo "数据库下载链接不存在，请添加3个Secrets，KEY为：url_db、url_db_shm和url_db_wal，VALUE分别对应data.db、data.db-shm和data.db-wal，三个数据库文件的下载地址。现在使用默认数据库。"
+        cp ./data/data.db* ../
+    fi
+else
+    echo "data.db 还在，不需要重新下载数据库。"
+fi
+
+./apan server --no-prefix
